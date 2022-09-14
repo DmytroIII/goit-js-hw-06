b04c7d510879acbdf1535b37800ede0e4785ddb2
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-09-15 01:28:48 +0300

    create task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let ingredient = document.querySelector('#ingredients');
+  
+for (let i = 0; i <= ingredients.length - 1; i++) {
+	let li = document.createElement('li');
+  li.innerHTML = ingredients[i];
+  li.classList.add('item');
+	ingredient.append(li);
+}
