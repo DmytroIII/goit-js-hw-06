ec0359b2f292bd014095fb03f8a316fd7f17c8aa
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-09-23 01:47:48 +0300

    tusk 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+
+
+
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', OnSubmit)
+function OnSubmit(evt) {
+    evt.preventDefault()
+    console.dir(evt.currentTarget)
+     const {email, password} = evt.currentTarget.elements;
+     const data = {
+        email: email.value,
+        password: password.value
+     }
+    
+    if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("Вы заполнили не все поля формы!");
+  }
+    
+     console.log(data);
+}
