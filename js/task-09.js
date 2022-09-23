a4f6ba05b9c5e496a464c4daaba4729032f6f33a
Filatov Dmytro <dmitrovfilatov@gmail.com>
2022-09-23 23:31:38 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
-function getRandomHexColor() {
+
+ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+
+const buttonAction = document.querySelector('.change-color');
+const bodyColor = document.querySelector("body");
+const spanText = document.querySelector(".color");
+
+buttonAction.addEventListener('click', () => { 
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  spanText.textContent = getRandomHexColor();
 }
+)
+
